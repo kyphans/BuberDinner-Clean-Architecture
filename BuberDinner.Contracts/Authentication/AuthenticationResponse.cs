1076f972c12ed1e08cfc9a2fd00d81a098e39146
Ky Phan <kyphanz1@gmail.com>
2023-10-06 18:02:14 +0700

    .

---
+++ BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Contracts.Authentication
+{
+    public record AuthenticationResponse(Guid Id, string FristName, string LastName, string Email, string Token);
+
+}
